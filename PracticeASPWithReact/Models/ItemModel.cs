4688d3b959f68c1e7c1eb2ba43611659e27e15eb
Haroon Tahir <haroontahirr100@gmail.com>
2023-02-24 01:51:04 +0500

    Add project files.

---
+++ PracticeASPWithReact/Models/ItemModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PracticeASPWithReact.Models {
+	public class ItemModel {
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public int ImageId { get; set; }
+		public int Ranking { get; set; }
+		public int ItemType { get; set; }
+	}
+}
